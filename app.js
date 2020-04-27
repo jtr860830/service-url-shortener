7255a41aec1bb015344c8c0ca6847633af184f75
Jezrien Hsieh <jtr860830@gmail.com>
2020-04-27 15:53:51 +0800

    Fix typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Koa = require("koa");
 const routes = require("./routes");
 const config = require("./config");
 
-const app = Koa();
+const app = new Koa();
 app.use(routes);
 
 app.listen(config.port, config.host);
